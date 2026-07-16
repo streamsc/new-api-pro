6f84ab0c2ea76e6cfedf25b1fc034a0a8d42a807
streamsc <39050063+streamsc@users.noreply.github.com>
2026-07-16 16:57:39 +0800

    test: cover multipart audio streaming requests
    (cherry picked from commit fc63b9414ce3e7906701edf789efb809d1fc3820)
    (cherry picked from commit 785c12beefc3032f74de76fe43ec34fec6a09414)
    (cherry picked from commit eaae83372de78241d2913e8425ff851f70ffcc62)

---
+++ relay/helper/audio_request_test.go
@@ -0,0 +1,33 @@
+package helper
+
+import (
+	"bytes"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	relayconstant "github.com/QuantumNous/new-api/relay/constant"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAndValidAudioRequestMultipartStream(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	var body bytes.Buffer
+	writer := multipart.NewWriter(&body)
+	require.NoError(t, writer.WriteField("model", "whisper-1"))
+	require.NoError(t, writer.WriteField("stream", "true"))
+	require.NoError(t, writer.Close())
+
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+	c.Request = httptest.NewRequest(http.MethodPost, "/v1/audio/transcriptions", &body)
+	c.Request.Header.Set("Content-Type", writer.FormDataContentType())
+
+	request, err := GetAndValidAudioRequest(c, relayconstant.RelayModeAudioTranscription)
+	require.NoError(t, err)
+	require.NotNil(t, request.Stream)
+	require.True(t, bool(*request.Stream))
+	require.True(t, request.IsStream(c))
+}
